62ba635387ce5f60669588fcd3b6181d79b61990
John Holdsworth <github@johnholdsworth.com>
2017-05-23 12:15:02 +0100

    Updated for 2017

--- swifthello/src/main/swift/Sources/main.swift
+++ swifthello/src/main/swift/Sources/main.swift
@@ -1,6 +1,6 @@
 
 import java_swift
-import Foundation
+@_exported import Foundation
 import Dispatch
 
 // link back to Java side of Application
@@ -10,7 +10,7 @@ var responder: SwiftHello_ResponderForward!
 @_silgen_name("Java_net_zhuoweizhang_swifthello_SwiftHello_bind")
 public func bind( __env: UnsafeMutablePointer<JNIEnv?>, __this: jobject?, __self: jobject? )-> jobject? {
     // required when using Java from Dispatch
-    DispatchGroup.threadCleanupCallback = JNI_DetachCurrentThread
+    DispatchQueue.threadCleanupCallback = JNI_DetachCurrentThread
     // This Swift instance forwards to Java through JNI
     responder = SwiftHello_ResponderForward( javaObject: __self )
     // This Swift instance receives native calls from Java
@@ -20,21 +20,22 @@ public func bind( __env: UnsafeMutablePointer<JNIEnv?>, __this: jobject?, __self
 class ListenerImpl: SwiftHello_ListenerBase {
 
     // incoming from Java activity
-    override func processNumber( number: Double ) {
+    override func processNumber( arg0: Double ) {
         // outgoing back to Java
-        responder.processedNumber( number+42.0 )
+        responder.processedNumber( arg0+42.0 )
     }
 
     // incoming from Java activity
-    override func processText( text: String? ) {
-        for _ in 0..<1000 {
+    override func processText( arg0: String? ) {
+        for _ in 0..<100 {
             let tester = responder.testResponder()!
             SwiftTestResponder().respond( to: tester )
         }
-        processText( text!, initial: true )
+        processText( arg0!, initial: true )
     }
 
     static var thread = 0
+    let url = URL( string: "http://www.bbc.co.uk/" )!
 
     func processText( _ text: String, initial: Bool ) {
         var out = [String]()
@@ -43,13 +44,15 @@ class ListenerImpl: SwiftHello_ListenerBase {
         }
         do {
             NSLog( "Fetch" )
-            let url = URL( string: "http://www.bbc.co.uk/news" )!
-            let input = try NSString( contentsOf: url,
-                                      encoding: String.Encoding.utf8.rawValue )
+	    var enc: UInt = 0
+            let input = try NSString( contentsOf: url, usedEncoding: &enc )
+//                                      encoding: String.Encoding.utf8.rawValue )
             NSLog( "Match" )
-            let regexp = try RegularExpression(pattern:"(\\w+)", options:[])
+            let regexp = try NSRegularExpression(pattern:"(\\w+)", options:[])
+            NSLog( "Match2" )
             for match in regexp.matches(in: String(describing: input), options: [],
                                         range: NSMakeRange(0,input.length) ) {
+
                                             out.append( ""+input.substring(with:match.range) )
             }
             NSLog( "Display" )
@@ -64,9 +67,9 @@ class ListenerImpl: SwiftHello_ListenerBase {
             ListenerImpl.thread += 1
             let background = ListenerImpl.thread
             DispatchQueue.global().async {
-                for i in 1..<100000 {
+                for i in 1..<5 {
                     NSLog("Sleeping")
-                    sleep(20)
+                    sleep(1)
                     // outgoing back to Java
                     _ = responder.debug( "Process \(background)/\(i)" )
                     _ = self.processText("World #\(i)", initial: false)

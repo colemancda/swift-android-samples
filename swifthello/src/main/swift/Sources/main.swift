bf1fcf13aac51828f134e82a33c2bfa616425587
John Holdsworth <github@johnholdsworth.com>
2017-05-23 16:46:48 +0100

    Updated for 2017

--- swifthello/src/main/swift/Sources/main.swift
+++ swifthello/src/main/swift/Sources/main.swift
@@ -10,7 +10,7 @@ var responder: SwiftHello_ResponderForward!
 @_silgen_name("Java_net_zhuoweizhang_swifthello_SwiftHello_bind")
 public func bind( __env: UnsafeMutablePointer<JNIEnv?>, __this: jobject?, __self: jobject? )-> jobject? {
     // required when using Java from Dispatch
-    DispatchGroup.threadCleanupCallback = JNI_DetachCurrentThread
+    DispatchQueue.threadCleanupCallback = JNI_DetachCurrentThread
     // This Swift instance forwards to Java through JNI
     responder = SwiftHello_ResponderForward( javaObject: __self )
     // This Swift instance receives native calls from Java
@@ -47,7 +47,7 @@ class ListenerImpl: SwiftHello_ListenerBase {
             let input = try NSString( contentsOf: url,
                                       encoding: String.Encoding.utf8.rawValue )
             NSLog( "Match" )
-            let regexp = try RegularExpression(pattern:"(\\w+)", options:[])
+            let regexp = try NSRegularExpression(pattern:"(\\w+)", options:[])
             for match in regexp.matches(in: String(describing: input), options: [],
                                         range: NSMakeRange(0,input.length) ) {
                                             out.append( ""+input.substring(with:match.range) )
@@ -66,7 +66,7 @@ class ListenerImpl: SwiftHello_ListenerBase {
             DispatchQueue.global().async {
                 for i in 1..<100000 {
                     NSLog("Sleeping")
-                    sleep(20)
+                    sleep(2)
                     // outgoing back to Java
                     _ = responder.debug( "Process \(background)/\(i)" )
                     _ = self.processText("World #\(i)", initial: false)

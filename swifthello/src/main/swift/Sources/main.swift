44b9e658da7000a7b08cd911318e0f0efc4e6e5a
John Holdsworth <github@johnholdsworth.com>
2017-05-23 17:16:52 +0100

    Updated for 2017

--- swifthello/src/main/swift/Sources/main.swift
+++ swifthello/src/main/swift/Sources/main.swift
@@ -9,8 +9,6 @@ var responder: SwiftHello_ResponderForward!
 // one-off call to bind the Java and Swift sections of app
 @_silgen_name("Java_net_zhuoweizhang_swifthello_SwiftHello_bind")
 public func bind( __env: UnsafeMutablePointer<JNIEnv?>, __this: jobject?, __self: jobject? )-> jobject? {
-    // required when using Java from Dispatch
-    DispatchQueue.threadCleanupCallback = JNI_DetachCurrentThread
     // This Swift instance forwards to Java through JNI
     responder = SwiftHello_ResponderForward( javaObject: __self )
     // This Swift instance receives native calls from Java
